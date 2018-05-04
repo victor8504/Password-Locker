a6b70a704aa6a7c7fcd32991ba39c4e7feaf2e5c
victor njuguna <victormoore254@gmail.com>
2018-05-04 16:43:29 +0300

    create test to save created accounts

--- tester_test.py
+++ tester_test.py
@@ -1,5 +1,5 @@
 import unittest # Importing the unittest module
-from tester import User
+from tester import User #Importing the user class
 
 class TestUser(unittest.TestCase):
 
@@ -18,15 +18,25 @@ class TestUser(unittest.TestCase):
 
         self.new_account = User("Victor Njuguna", "victormoore254@gmail.com", "victor8504") # create user object
 
-        def test_init(self):
-            
-            '''
-            test_init test case to test if the object is initialized properly
-            '''
+    def test_init(self):
+        
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
 
-            self.assertEqual(self.new_user.account_name,"Victor Njuguna")
-            self.assertEqual(self.new_user.email_address,"victormoore254@gmail.com") 
-            self.assertEqual(self.new_user.password,"victor8504")
+        self.assertEqual(self.new_account.account_name,"Victor Njuguna")
+        self.assertEqual(self.new_account.email_address,"victormoore254@gmail.com") 
+        self.assertEqual(self.new_account.password,"victor8504")
+
+    def test_save_account(self):
+        
+        '''
+        test_save_account test case to test if the user object is saved into
+        the user list
+        '''
+
+        self.new_account.save_account() # saving the new account
+        self.assertEqual(len(User.user_list),1) 
 
 if __name__ == '__main__':
     unittest.main()
