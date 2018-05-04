6050e6eda7baa4655fa4fbb83de63b620de7a09f
victor njuguna <victormoore254@gmail.com>
2018-05-04 17:08:33 +0300

    create a save_account method that saves user objects into user_list

--- tester.py
+++ tester.py
@@ -20,3 +20,11 @@ class User:
         self.account_name = account_name
         self.email_address = email_address
         self.password = password
+
+    def save_account(self):
+
+        '''
+        save_contact method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
