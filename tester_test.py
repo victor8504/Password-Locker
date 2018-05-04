6e9321c3749a64d5a64a73556c12b1574d67d642
victor njuguna <victormoore254@gmail.com>
2018-05-04 17:52:29 +0300

    create test to save multiple accounts in the user_list

--- tester_test.py
+++ tester_test.py
@@ -31,12 +31,24 @@ class TestUser(unittest.TestCase):
     def test_save_account(self):
         
         '''
-        test_save_account test case to test if the user object is saved into
+        test_save_account test case to test if the account object is saved into
         the user list
         '''
 
         self.new_account.save_account() # saving the new account
         self.assertEqual(len(User.user_list),1) 
+
+    def test_save_multiple_account(self):
+
+        '''
+        test_save_multiple_account to check if we can save multiple account
+        objects to our user_list
+        '''
+
+        self.new_account.save_account()
+        test_account = User("Michy Bats", "michybats@gmail.com", "michybats") # new account
+        test_account.save_account()
+        self.assertEqual(len(User.user_list),2)
 
 if __name__ == '__main__':
     unittest.main()
