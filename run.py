68523936904d2757ef2101448a1fc41c196e3005
victor njuguna <victormoore254@gmail.com>
2018-05-06 15:47:44 +0300

    create a function called create_account

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.6
+from tester import User #Importing the user class
+
+def create_account(accname,email,password):
+
+    '''
+    Function to create a new account
+    '''
+
+    new_account = User(accname,email,password)
+    return new_account
